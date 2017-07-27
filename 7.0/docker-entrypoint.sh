5ecdc08e9d49a3bbd798a0a532aed17e97350a77
Julian Haupt <julian.haupt@hauptmedia.de>
2017-07-27 17:26:31 +0200

    fixed paths to php7 ini file

--- 7.0/docker-entrypoint.sh
+++ 7.0/docker-entrypoint.sh
@@ -7,10 +7,10 @@ fi
 echo $TIMEZONE > /etc/timezone && \
 dpkg-reconfigure -f noninteractive tzdata
 
-sed -i "s|;date.timezone =.*|date.timezone = $TIMEZONE|" /etc/php5/*/php.ini
+sed -i "s|;date.timezone =.*|date.timezone = $TIMEZONE|" /etc/php/7.0/*/php.ini
 
 if [ -n "$PHP_MEMORY_LIMIT" ]; then
-	sed -i "s/^memory_limit =.*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php5/*/php.ini
+	sed -i "s/^memory_limit =.*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.0/*/php.ini
 fi
 
 exec "$@"
