c6052c6d8699431e59c1b520007d9970a380d100
Julian Haupt <julian.haupt@hauptmedia.de>
2017-08-04 10:06:54 +0200

    added php 7.1 target

---
+++ 7.1/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$TIMEZONE" ]; then
+	TIMEZONE=Etc/UTC
+fi
+
+echo $TIMEZONE > /etc/timezone && \
+dpkg-reconfigure -f noninteractive tzdata
+
+sed -i "s|;date.timezone =.*|date.timezone = $TIMEZONE|" /etc/php/7.1/*/php.ini
+
+if [ -n "$PHP_MEMORY_LIMIT" ]; then
+	sed -i "s/^memory_limit =.*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.1/*/php.ini
+fi
+
+exec "$@"
